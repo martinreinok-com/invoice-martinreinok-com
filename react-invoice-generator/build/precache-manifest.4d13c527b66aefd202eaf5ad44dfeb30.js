baa4c47d4ac974d9577b98d8a25ed2b881956c3f
Martin Reinok <martin.reinok@gmail.com>
2022-09-09 01:14:30 +0200

    Added build

---
+++ react-invoice-generator/build/precache-manifest.4d13c527b66aefd202eaf5ad44dfeb30.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b2db4aec667d9a01c9ea089f65fceb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81674e6d46d1c01f0074",
+    "url": "/static/css/2.0d77bc6e.chunk.css"
+  },
+  {
+    "revision": "4331de7383e3036c37f9",
+    "url": "/static/css/main.88a803d5.chunk.css"
+  },
+  {
+    "revision": "81674e6d46d1c01f0074",
+    "url": "/static/js/2.9b78b7d6.chunk.js"
+  },
+  {
+    "revision": "5fae47d0a016a5b68e2af829b08bec2a",
+    "url": "/static/js/2.9b78b7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4331de7383e3036c37f9",
+    "url": "/static/js/main.e41499dc.chunk.js"
+  },
+  {
+    "revision": "c8d6aca6dda5a378a01d",
+    "url": "/static/js/runtime-main.6ee9ec60.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/static/media/resize.d4da359f.svg"
+  }
+]);
