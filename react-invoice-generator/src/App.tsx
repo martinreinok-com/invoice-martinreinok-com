e9a50cb676aba1f6e4a7c75af304f2fa20f0087d
Martin Reinok <78325056+martinreinok@users.noreply.github.com>
2022-09-09 09:04:38 +0200

    Update App.tsx

--- react-invoice-generator/src/App.tsx
+++ react-invoice-generator/src/App.tsx
@@ -20,9 +20,10 @@ function App() {
     <div className="app">
       <h3 className="center fs-10">Here shall be link/history to previously generated invoices (cross-user, with login/token?)</h3>
       <h2 className="center fs-10">Other ideas: </h2>
-      <h3 className="center fs-10">Language button</h3>
-      <h3 className="center fs-10">Make the download button 'normal' and accessible when browser compacted</h3>
-      <h3 className="center fs-10">Saveable templates for companies? for a case of a reoccuring client</h3>
+      <h3 className="center fs-10">Language converting button</h3>
+      <h3 className="center fs-10">Make the download button 'better'</h3>
+      <h3 className="center fs-10">Saveable templates for companies? Searchable be company name or ID</h3>
+      <h3 className="center fs-10">The "remove line" button is not visible on phone</h3>
       <InvoicePage data={data} onChange={onInvoiceUpdated} />
       <h3 className="center fs-10">Code and design by: <a href="https://github.com/tuanpham-dev" target="_blank">tuanpham-dev (GitHub)</a>, slightly modded by <a href="https://github.com/martinreinok" target="_blank">me</a> </h3>
       <h4 className="center fs-10"></h4>
